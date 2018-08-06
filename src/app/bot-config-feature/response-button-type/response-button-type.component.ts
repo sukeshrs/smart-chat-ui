37a28fe8eb3a524a6b8800ae68fa428f82368e74
victoriaagb <victoriaagb93@gmail.com>
2018-08-06 16:07:42 -0400

    added implemented remove button functionality

--- src/app/bot-config-feature/response-button-type/response-button-type.component.ts
+++ src/app/bot-config-feature/response-button-type/response-button-type.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 import { Input, Output, EventEmitter } from '@angular/core';
 import { Topic } from '../../model/topic.model';
 import { Button } from '../../model/topic/button.model';
@@ -12,15 +13,36 @@ import { SmartChatModel } from "../../model/smart-chat-model.service";
 export class ResponseButtonTypeComponent implements OnInit {
 
   public buttons: Button[];
-  constructor(private smartChatModel: SmartChatModel) { }
+  navigationSubscription;
+
+  constructor(
+    private smartChatModel: SmartChatModel,
+    private route: ActivatedRoute,
+    private router: Router) {
+      this.navigationSubscription = this.router.events.subscribe((e: any) => {
+        if (e instanceof NavigationEnd) {
+          this.initilizeInvites();
+        }
+      });
+    }
 
   ngOnInit() {
+    this.initilizeInvites();
+  }
+
+  ngOnDestroy() {
+    if (this.navigationSubscription) {
+       this.navigationSubscription.unsubscribe();
+    }
+  }
+
+  initilizeInvites(){
     this.buttons=this.smartChatModel.currentTopic.answers[0].attachment.payload.buttons;
     if(!this.buttons){
       this.buttons = [];
     }
   }
-
+  
   addButton(){
     let newButton: Button;
     newButton = {
@@ -30,6 +52,10 @@ export class ResponseButtonTypeComponent implements OnInit {
       payload:""
     }
     this.buttons.push(newButton);
+  }
+
+  removeButton(i: number){
+    this.buttons.splice(i,1);
   }
 
 }
